8cb07202d9d3503a106a708f591eb137cc687aa2
Nicholas Byrne <s160422@ad.aie.edu>
2017-02-02 17:08:08 +1100

    In work broken stuff

---
+++ HH Prototype/Assets/Scripts/NodeBasedEditor.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class NodeBasedEditor : EditorWindow
+{
+    private List<Node> nodes;
+
+    private GUIStyle nodeStyle;
+
+    [MenuItem("Window/Dialogue Editor")]
+    private static void OpenWindow()
+    {
+        NodeBasedEditor window = GetWindow<NodeBasedEditor>();
+        window.titleContent.text = "Dialogue Editor";
+        //window.title = "Dialogue Editor";
+    }
+
+    private void OnEnable()
+    {
+        nodeStyle = new GUIStyle();
+        nodeStyle.normal.background = EditorGUIUtility.Load("builtin skins/darkskin/images/node1.png") as Texture2D;
+        nodeStyle.border = new RectOffset(12, 12, 12, 12);
+    }
+
+    private void OnGUI()
+    {
+        DrawNodes();
+        ProcessNodeEvents(Event.current);
+        ProcessEvents(Event.current);
+        if (GUI.changed)
+            Repaint();
+    }
+
+    private void DrawNodes()
+    {
+        if (nodes != null)
+        {
+            foreach(Node node in nodes)
+            {
+                node.Draw();
+            }
+        }
+    }
+
+    private void ProcessEvents(Event e)
+    {
+        switch (e.type)
+        {
+            case EventType.MouseDown:
+                if (e.button == 1)
+                {
+                    ProcessContextMenu(e.mousePosition);
+                }
+                break;
+        }
+    }
+
+    private void ProcessNodeEvents(Event e)
+    {
+        if (nodes != null)
+        {
+            for (int i = nodes.Count - 1; i >= 0; --i)
+            {
+                bool guiChanged = nodes[i].ProcessEvents(e);
+                if (guiChanged)
+                {
+                    GUI.changed = true;
+                }
+            }
+        }
+    }
+
+    private void ProcessContextMenu(Vector2 mousePosition)
+    {
+        GenericMenu genericMenu = new GenericMenu();
+        genericMenu.AddItem(new GUIContent("Add Node"), false, () => OnClickAddNode(mousePosition));
+        genericMenu.ShowAsContext();
+    }
+
+    private void OnClickAddNode(Vector2 mousePosition)
+    {
+        if (nodes == null)
+        {
+            nodes = new List<Node>();
+        }
+
+        nodes.Add(new Node(mousePosition, 200, 50, nodeStyle));
+    }
+
+       
+}
