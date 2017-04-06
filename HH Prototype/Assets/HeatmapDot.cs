3f3bc25cbb96ede6e628dccbbb6dd052fac553fb
Kayne Palmer <kaynempalmer@gmail.com>
2017-04-06 13:14:24 +1000

    added Trough, added heatMap

---
+++ HH Prototype/Assets/HeatmapDot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatmapDot : MonoBehaviour
+{
+
+  public  float count;
+    Color col;
+    // Use this for initialization
+    void Start()
+    {
+        count = 0;
+        col = GetComponent<Renderer>().material.GetColor("_MainColor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            GetComponent<Renderer>().material.SetColor("_MainColor", new Color(col.r, col.g - (count/255f), col.b, col.a));
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("HeatDot"))
+        {
+
+            //GetComponent<Renderer>().material.color = new Color(col.r, col.g - (count), col.b, col.a);
+            count+= 10;
+        }
+    }
+}
---
+++ HH Prototype/Assets/MovementTracker.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System;
+
+public class MovementTracker : MonoBehaviour
+{
+    [SerializeField]
+    public Vector3[] pos;
+    public float timer;
+    public float rate = 2;
+    int count;
+    public GameObject Dot;
+    int posSize;
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = rate;
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            timer = rate;
+            //   pos.Add(new Vector2(PlayerInventory.instance.transform.position.x, PlayerInventory.instance.transform.position.z));
+            Vector3[] newpos = new Vector3[pos.Length + 1];
+            for (int i = 0; i < pos.Length; i++)
+            {
+                newpos[i] = pos[i];
+            }
+            pos = newpos;
+            posSize = pos.Length;
+            pos[posSize - 1] = (PlayerInventory.instance.transform.position);
+            // Instantiate(Dot, new Vector3(PlayerInventory.instance.transform.position.x, (int)PlayerInventory.instance.transform.position.y, PlayerInventory.instance.transform.position.z), Quaternion.identity);
+            count++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Save();
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Load();
+        }
+
+    }
+
+    void OnApplicationClose()
+    {
+        Save();
+    }
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+
+        SaveData data = new SaveData();
+
+        data.posSize = posSize;
+
+        data.posX = new float[posSize];
+        data.posY = new float[posSize];
+        data.posZ = new float[posSize];
+
+        for (int i = 0; i < posSize; i++)
+        {
+            data.posX[i] = pos[i].x;
+            data.posY[i] = pos[i].y;
+            data.posZ[i] = pos[i].z;
+        }
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+            SaveData data = (SaveData)bf.Deserialize(file);
+            file.Close();
+
+            posSize = data.posSize;
+            
+            pos = new Vector3[posSize];
+
+
+            for (int i = 0; i < posSize; i++)
+            {
+
+                pos[i].x = data.posX[i];
+                pos[i].y = data.posY[i];
+                pos[i].z = data.posZ[i];
+            }
+            //pos.Add(new Vector3(data.posX, data.posY, data.posZ));
+            //  pos.Clear();
+            // pos.Add(new Vector3(data.posX[i], data.posY[i], data.posZ[i]));
+
+
+            for (int i = 0; i < pos.Length; i++)
+            {
+                Instantiate(Dot, pos[i], transform.rotation);
+            }
+        }
+    }
+
+    [Serializable]
+    class SaveData
+    {
+        public int posSize;
+        public float[] posX;
+        public float[] posY;
+        public float[] posZ;
+    }
+
+}
